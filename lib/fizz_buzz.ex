a194d38735d042eb811b61b467613c1dbad4fac3
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-12-12 14:35:10 -0300

    feat: handle file read errors in `FizzBuzz.build/1` and update tests

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -3,7 +3,10 @@ defmodule FizzBuzz do
     File.read(file_name) |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    String.split(result, ",") |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 end
 
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -2,13 +2,18 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
 
   test "build/1 reads a file" do
-    assert {:ok, expected_result} = File.read("numbers.txt")
+    assert {:ok, file} = File.read("numbers.txt")
+    expected_result = String.split(file, ",") |> Enum.map(&String.to_integer/1)
+
     assert FizzBuzz.build("numbers.txt") == expected_result
   end
 
   test "build/1 returns error when file does not exist" do
-    assert reason = FizzBuzz.build("non_existent.txt")
-    assert reason == :enoent
+    assert FizzBuzz.build("non_existent.txt") == "Error reading the file: enoent"
+  end
+
+  test "handle_file_read/1 should return list of integers" do
+    assert FizzBuzz.handle_file_read({:ok, "1,2,3"}) == [1, 2, 3]
   end
 end
 
